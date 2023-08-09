31faed951425297c272ec4b8d98082ee055f8f11
Gabriel <gabriel.forcellini@hotmail.com>
2023-08-09 11:08:02 -0300

    adicionando script de build

--- db/conn.js
+++ db/conn.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 async function main() {
     const DB_USER = process.env.DB_USER;
     const DB_PASSWORD = process.env.DB_PASSWORD;
-    
+
     await mongoose
         .connect(
-            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@api-rest.h9mcxap.mongodb.net/api-rest?retryWrites=true&w=majority`
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@api-rest.h9mcxap.mongodb.net/?retryWrites=true&w=majority`
         )
-        console.log("Conectado ao mongoDB");
+    console.log("Conectado ao mongoDB");
 };
 
 main().catch((err) => console.log(err));
